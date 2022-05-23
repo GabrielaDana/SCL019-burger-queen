094742f38527c7a8ec9ad7cac403680456aa4b95
GabrielaDana <gabriela.dana.pacheco@gmail.com>
2022-05-23 18:18:30 -0400

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
-
-import RouterApp from './RouterApp.jsx'; 
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate
+} from 'react-router-dom';
+import { TakeOrder } from './components/TakeOrder.jsx'
+import { Home } from './components/Home.jsx'
+import { Kitchen } from './components/Kitchen.jsx';
 
 
 function App() {
   return (
     <div>
-     <RouterApp />
+      <Router>
+          <Routes>
+            <Route path="/takeOrder" element={<TakeOrder />}/>
+            <Route path="/kitchen" element={<Kitchen />}/>
+            <Route path="/" element={<Home />}/>
+            <Route path="*" element={<Navigate to='/' />}/>
+          </Routes>
+      </Router>
     </div>
   );
 } 
