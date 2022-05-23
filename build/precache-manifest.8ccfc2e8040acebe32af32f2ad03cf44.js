0c5a3c392513687c1c1bb87741d37456fec64edb
GabrielaDana <gabriela.dana.pacheco@gmail.com>
2022-05-22 20:30:52 -0400

    first commit

---
+++ build/precache-manifest.8ccfc2e8040acebe32af32f2ad03cf44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bab94272b5890d05cfc",
+    "url": "/GabrielaDana/SCL019-burger-queen/static/css/main.a66cfd42.chunk.css"
+  },
+  {
+    "revision": "1bab94272b5890d05cfc",
+    "url": "/GabrielaDana/SCL019-burger-queen/static/js/main.1bab9427.chunk.js"
+  },
+  {
+    "revision": "2d3efeb71c7eebfe7cd0",
+    "url": "/GabrielaDana/SCL019-burger-queen/static/js/1.2d3efeb7.chunk.js"
+  },
+  {
+    "revision": "6a21bf83ebfabef424ec",
+    "url": "/GabrielaDana/SCL019-burger-queen/static/js/2.6a21bf83.chunk.js"
+  },
+  {
+    "revision": "feb870b33e80c7ce3b51",
+    "url": "/GabrielaDana/SCL019-burger-queen/static/js/runtime~main.feb870b3.js"
+  },
+  {
+    "revision": "6d7d6bfaf5a4b32e0255642a4a75d1d0",
+    "url": "/GabrielaDana/SCL019-burger-queen/index.html"
+  }
+];
